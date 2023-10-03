21d1bcfbe52a2e459c6579d9cc2491ecb1af8b03
Frances Morales Velilla <frances0688@gmail.com>
2023-10-03 15:58:11 +0200

    complete EXTRA version 2: using an object array.  Some corretions for form reset and removed clear function redundancy

--- main.js
+++ main.js
@@ -28,38 +28,64 @@ const btn = document.getElementById('btn');
 
 // Exercise 2 : EXTRA
 
-const user = {
-    name : [],
-    email : [],
-    message : []
-};
+// const user = {
+//     name : [],
+//     email : [],
+//     message : []
+// };
+
+// function createDataObject(e) {
+//     e.preventDefault();
+    
+//     user.name.push(userName.value);
+//     user.email.push(userEmail.value);
+//     user.message.push(userMessage.value);
+
+//     console.log(user); 
+//     printObjectInfo(user);
+//     document.getElementById('myForm').reset();
+// }
+
+// function printObjectInfo (user){
+//     localStorage.user = JSON.stringify(user);
+//     const storedUser = JSON.parse(localStorage.getItem("user"));
+//     const newPar = document.createElement('p');
+    
+//     for (let i=0; i<user.name.length; i++){
+//         newPar.innerHTML = `User #${i+1}<br>Name: ${storedUser.name[i]}<br>Email: ${storedUser.email[i]}<br>Message: ${storedUser.message[i]}`;
+//         document.querySelector('body').appendChild(newPar);
+//     }
+// }
+
+
+// EXTRA - Version 2
+
+const allUsers = []
 
 function createDataObject(e) {
     e.preventDefault();
     
-    user.name.push(userName.value);
-    user.email.push(userEmail.value);
-    user.message.push(userMessage.value);
-
-    console.log(user); 
-    printObjectInfo(user);
-    document.getElementsByClassName('input').reset();
-}
-
-function printObjectInfo (user){
-    localStorage.user = JSON.stringify(user);
-    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const user = {
+        name : userName.value,
+        email : userEmail.value,
+        message : userMessage.value
+    };
+    allUsers.push(user)
+    
+    
+    localStorage.allUsers = JSON.stringify(allUsers);
+    
+    const storedUsers = JSON.parse(localStorage.getItem("allUsers"));
     const newPar = document.createElement('p');
     
-    for (let i=0; i<user.name.length; i++){
-        newPar.innerHTML = `User #${i+1}<br>Name: ${storedUser.name[i]}<br>Email: ${storedUser.email[i]}<br>Message: ${storedUser.message[i]}`;
+    for (let i=0; i<storedUsers.length; i++){
+        newPar.innerHTML = `User #${i+1}<br>Name: ${storedUsers[i].name}<br>Email: ${storedUsers[i].email}<br>Message: ${storedUsers[i].message}`;
         document.querySelector('body').appendChild(newPar);
     }
+   
+    document.getElementById('myForm').reset();
 }
 
-function clearLocalStorage() {
-    localStorage.clear()
-}
 
 btn.addEventListener('click', createDataObject);
-clear.addEventListener('click', clearLocalStorage)
+clear.addEventListener('click', localStorage.clear())
